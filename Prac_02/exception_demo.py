e0d16077b4aa6fa7128dcaad7b11bc740be4343b
NammanC137 <102642165+JC827574@users.noreply.github.com>
2022-04-02 19:42:43 +0800

    Add files via upload

---
+++ Prac_02/exception_demo.py
@@ -0,0 +1,15 @@
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+print("Finished.")
+
+# A value error may occur when you try to enter a object other than a numeric in the input option
+
+# When the input in the denominator is 0
+
+
